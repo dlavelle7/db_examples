48419881cd63420ff0fd6ed4c3fd6411cc833bc4
David Lavelle <davidlavelle1@hotmail.com>
2019-02-17 18:27:45 +0000

    basic count example

--- PostgreSQL/scripts/aggregates.sql
+++ PostgreSQL/scripts/aggregates.sql
@@ -1,1 +1,7 @@
-# TODO: COUNT, MIN, MAX, GROUP BY, etc.
+-- TODO: Aggregate function examples: MIN, MAX, GROUP BY, etc.
+
+-- COUNT() Example - Count the number of students in each course
+SELECT course_id, COUNT(course_id) AS students_enrolled
+FROM enrollment
+GROUP BY course_id
+ORDER BY course_id
