7c4dc743040f1a829d1a940e22d1ee78fd8caa83
David Lavelle <davidlavelle1@hotmail.com>
2019-02-17 17:19:39 +0000

    basic join example done

--- PostgreSQL/scripts/joins.sql
+++ PostgreSQL/scripts/joins.sql
@@ -1,6 +1,10 @@
 -- TODO: Examples of all joins
 
--- Get details of all students in the Maths course
-SELECT * FROM student
+-- Get details of students enrolled in the Maths course
+SELECT student.first_name, enrollment.enrolled_on, course.name AS course_name
+FROM student
 INNER JOIN enrollment
-ON enrollment.student_id = student.id
+    ON enrollment.student_id = student.id
+INNER JOIN course
+    ON course.id = enrollment.course_id
+WHERE course.name = 'Maths'
